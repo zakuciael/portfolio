ac40cb701fc4882f7d9bdfc0a96e7cdafddc75ce
Krzysztof Saczuk <zakuciael@outlook.com>
2020-07-10 01:53:41 +0200

    Updated social media.

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -3,7 +3,7 @@ import {Avatar} from "./Avatar";
 import {SimpleTitle} from "./SimpleTitle";
 import {Footer} from "./Footer";
 import avatar from "../img/avatar.png"
-import {faDiscord, faFacebook, faGithub, faGitlab, faTwitter} from "@fortawesome/free-brands-svg-icons";
+import {faDiscord, faGithub, faLinkedinIn, faTwitter} from "@fortawesome/free-brands-svg-icons";
 import {SocialMediaIcon} from "./SocialMediaIcon";
 
 class Root extends React.Component {
@@ -15,10 +15,9 @@ class Root extends React.Component {
                 <span className="separator" />
                 <div className="icons-container">
                     <SocialMediaIcon icon={faDiscord}  text="ZaKKu#0001"  name="Discord"/>
-                    <SocialMediaIcon icon={faFacebook}  url="https://www.facebook.com/dev.zakku/" text="Facebook"  name="Facebook"/>
                     <SocialMediaIcon icon={faTwitter}  url="https://twitter.com/zakuciael" text="Twitter"  name="Twitter"/>
-                    <SocialMediaIcon icon={faGitlab}  url="https://gitlab.com/zakuciael" text="GitLab"  name="GitLab"/>
                     <SocialMediaIcon icon={faGithub}  url="https://github.com/zakuciael" text="GitHub" name="GitHub"/>
+                    <SocialMediaIcon icon={faLinkedinIn} url="https://www.linkedin.com/in/zakuciael/" text="LinkedIn" name="LinkedIn" />
                 </div>
                 <Footer />
             </div>
