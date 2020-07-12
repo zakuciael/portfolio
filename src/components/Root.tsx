34fd13faf9d46d1a7dbe45742e10cde5a43b7d2a
Krzysztof Saczuk <zakuciael@outlook.com>
2020-07-12 18:04:35 +0200

    Added paypal social media icon.

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import {Avatar} from "./Avatar";
-import {SimpleTitle} from "./SimpleTitle";
-import {Footer} from "./Footer";
-import avatar from "../img/avatar.png"
-import {faDiscord, faGithub, faLinkedinIn, faTwitter} from "@fortawesome/free-brands-svg-icons";
+import {faDiscord, faGithub, faLinkedinIn, faPaypal, faTwitter} from "@fortawesome/free-brands-svg-icons";
 import {SocialMediaIcon} from "./SocialMediaIcon";
+import {SimpleTitle} from "./SimpleTitle";
+import avatar from "../img/avatar.png"
+import {Footer} from "./Footer";
+import {Avatar} from "./Avatar";
+import React from "react";
 
 class Root extends React.Component {
     render() {
@@ -17,6 +17,7 @@ class Root extends React.Component {
                     <SocialMediaIcon icon={faDiscord}  text="ZaKKu#0001"  name="Discord"/>
                     <SocialMediaIcon icon={faTwitter}  url="https://twitter.com/zakuciael" text="Twitter"  name="Twitter"/>
                     <SocialMediaIcon icon={faGithub}  url="https://github.com/zakuciael" text="GitHub" name="GitHub"/>
+                    <SocialMediaIcon icon={faPaypal}  url="https://paypal.me/zakuciael" text="PayPal" name="PayPal" />
                     <SocialMediaIcon icon={faLinkedinIn} url="https://www.linkedin.com/in/zakuciael/" text="LinkedIn" name="LinkedIn" />
                 </div>
                 <Footer />
